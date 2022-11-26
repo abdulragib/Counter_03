70475fb52c301a9639902708656623c7f39de6aa
Abdul Ragib <abdulragib36@gmail.com>
2022-11-26 21:23:59 +0530

    Solution 1.0

--- counter.js
+++ counter.js
@@ -1,22 +1,57 @@
-var value = document.getElementById("num").innerText;
 
 
-function plus(){
+var incrementCounter=document.getElementById("increment");
+var decrementCounter=document.getElementById("decrement");
+var resetCounter=document.getElementById("reset");
+var errorMsg=document.getElementById("error-msg");
+
+var counter = document.getElementById("num").innerText;
+
+incrementCounter.onclick=increment;
+decrementCounter.onclick=decrement;
+resetCounter.onclick=reset;
+
+
+function increment(){
+    
     var num = document.getElementById("num").innerHTML;
     num++;
     document.getElementById("num").innerHTML = num;
+    if(counter==0)
+    {
+       decrementCounter.disabled = false;
+    }
+    else{
+        errorMsg.innerText="";
+    }
 }
 
-function minus(){
+function decrement(){
+    
     var num = document.getElementById("num").innerHTML;
     num--;
     document.getElementById("num").innerHTML = num;
+
+    if(counter==0)
+    {
+        errorMsg.innerText="Hello one"
+        decrementCounter.disabled = true;
+    }
 }
 
-function clear(){
+function reset(){
     document.getElementById("num").innerHTML = 0;
+    if(counter==0)
+    {
+        errorMsg.innerText=" ";
+       decrementCounter.disabled = false;
+    }
 }
 
-var increment=plus;
-var decrement=minus;
-var reset=clear;
+
+
+
+
+
+
+
