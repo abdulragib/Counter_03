ff0e751b401be0fad97e6324b2976cb4a9900c61
Abdul Ragib <abdulragib36@gmail.com>
2022-11-27 18:40:46 +0530

    Solution 5.0

--- counter.js
+++ counter.js
@@ -23,21 +23,27 @@ function increment(){
     }
     else{
         errorMsg.style.display="none";
+        decrementCounter.disabled = false;
     }
 }
 
 function decrement(){
 
     var num = document.getElementById("num").innerHTML;
-    num--;
-    document.getElementById("num").innerHTML = num;
-
+   
+    
     if(num<=0)
     {
         errorMsg.style.display="block";
         errorMsg.innerText="Error : Cannot go below 0";
         decrementCounter.disabled = true;
-    } 
+    }
+    else{
+        num--;
+        document.getElementById("num").innerHTML = num;
+    }
+
+    
 }
 
 function reset(){
